d6625a453df3ee1ed999453913593b722ddc05b5
Nokulunga0 <magagulanokulunga38@gmail.com>
2023-08-25 11:12:01 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,11 +22,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new->str);
-		return (NULL);
-	}
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
